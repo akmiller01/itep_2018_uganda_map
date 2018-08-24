c51becf0da51f6117197138ffe38be29ff8883be
Alex Miller <alex.k.miller@gmail.com>
2018-08-24 13:40:47 -0400

    City markers

--- make_map.R
+++ make_map.R
@@ -10,12 +10,15 @@ dist.dat = read.csv("data/districts.csv")
 dist.dat$percent = dist.dat$percent*100
 ug = merge(ug,dist.dat,by="name")
 
+cities = read.csv("data/cities.csv")
+coordinates(cities)=~long+lat
+
 pal <- colorBin(
   palette = "YlOrRd",
   domain = ug@data[,"percent"]
 )
 
-m=leaflet(data=ug) %>%
+leaflet(data=ug) %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
   setView(33, 1, zoom=6) %>% 
   addPolygons(color = pal(ug@data[,"percent"])
@@ -26,5 +29,13 @@ m=leaflet(data=ug) %>%
                         "<b>",ug@data$name,"</b> <br/>",
                         "<b>Total FDI, 2012-2016 (US$ millions): </b>$",round(ug@data$fdi),"<br/>",
                         "<b>Percent: </b>",round(ug@data[,"percent"],2)
-                      )) %>%  
+                      )) %>%
+  addMarkers(
+    data=cities
+    ,popup=paste0(
+      "<b>",cities@data$city,"</b> <br/>",
+      "<b>District: </b>",cities@data$district,"<br/>",
+      "<b>Total FDI, 2012-2016 (US$ millions): </b>$",round(cities@data$fdi),"<br/>"
+    )
+    ) %>%
 addLegend("bottomright", pal=pal, values = ug@data[,"percent"], opacity = 1, title="Total FDI, 2012-2016",labFormat = labelFormat(suffix="%"))
