c66fa9fba3de8b1ab42d84d46cf56fb06b3e2669
Alex Miller <alex.k.miller@gmail.com>
2018-09-06 08:49:43 -0400

    Scale updates

--- make_eps.R
+++ make_eps.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("sp","rgdal","leaflet","data.table","ggplot2")
+list.of.packages <- c("sp","rgdal","leaflet","data.table","ggplot2","scales")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only=T)
@@ -6,9 +6,14 @@ source("https://raw.githubusercontent.com/akmiller01/alexm-util/master/DevInit/R
 
 setwd("~/git/itep_2018_uganda_map")
 
+reds = c("#FBD7CB","#F6B2A7","#F28E83","#ED695E","#E8443A")
+purples = c("#DEB5D6","#CD9DC8","#BC86BA","#AB6EAC","#9A579E","#893F90")
+
 ug = readOGR("shp/uganda.shp")
 dist.dat = read.csv("data/districts.csv")
-dist.dat$percent = NULL
+dist.dat$fdi = NULL
+setnames(dist.dat,"percent","fdi")
+dist.dat$fdi = dist.dat$fdi*100
 
 districts = ddw("spotlight_on_uganda.ref_uganda_district")
 districts$name[which(districts$name=="Sembabule")] = "Ssembabule"
@@ -40,9 +45,10 @@ ug.f = merge(ug.f,donor,by="name",all.x=T)
 
 ug.f = ug.f[order(ug.f$order),]
 
-palbins = quantile(pov$pov,seq(0,1,.2))
+palbins = c(30,50,70,90,97)
+names(palbins)=c("<30 %","30-50","50-70","70-90",">90 %")
 pal <- colorBin(
-  palette = "YlOrRd",
+  palette = reds,
   domain = pov$pov,
   na.color="#d0cccf",
   bins = palbins
@@ -50,12 +56,14 @@ pal <- colorBin(
 
 pov_map = 
   ggplot(ug.f)+
-  geom_polygon( aes(x=long,y=lat,group=group,fill=pov,color="grey",size=0.02))+
+  geom_polygon( aes(x=long,y=lat,group=group,fill=pov,color="#eeeeee",size=0.21))+
   coord_fixed(1) +
   scale_fill_gradientn(
+    na.value="#d0cccf",
     guide="legend",
-    breaks=unname(palbins),
-    colors=pal(palbins)
+    breaks=palbins,
+    colors=pal(palbins),
+    values=rescale(palbins)
   ) +
   scale_color_identity()+
   scale_size_identity()+
@@ -67,9 +75,10 @@ pov_map =
 ggsave(paste0("eps/pov.png"),pov_map,device="png",width=10,height=6)
 ggsave(paste0("eps/pov.eps"),pov_map,device="eps",width=10,height=6)
 
-palbins = quantile(local$local,seq(0,1,.2))
+palbins = c(1,2,3,4,10,17)
+names(palbins) = c("<1 %","1-2","2-3","3-4","4-10",">10 %")
 pal <- colorBin(
-  palette = "YlOrRd",
+  palette = purples,
   domain = local$local,
   na.color="#d0cccf",
   bins = palbins
@@ -77,12 +86,14 @@ pal <- colorBin(
 
 
 local_map = ggplot(ug.f)+
-  geom_polygon( aes(x=long,y=lat,group=group,fill=local,color="grey",size=0.02))+
+  geom_polygon( aes(x=long,y=lat,group=group,fill=local,color="#eeeeee",size=0.21))+
   coord_fixed(1) +
   scale_fill_gradientn(
+    na.value="#d0cccf",
     guide="legend",
-    breaks=unname(palbins),
-    colors=pal(palbins)
+    breaks=palbins,
+    colors=pal(palbins),
+    values=rescale(palbins)
   ) +
   scale_color_identity()+
   scale_size_identity()+
@@ -94,9 +105,10 @@ local_map = ggplot(ug.f)+
 ggsave(paste0("eps/local.png"),local_map,device="png",width=10,height=6)
 ggsave(paste0("eps/local.eps"),local_map,device="eps",width=10,height=6)
 
-palbins = quantile(donor$donor,seq(0,1,.2))
+palbins = c(1,2,3,4,10,30)
+names(palbins) = c("<1 %","1-2","2-3","3-4","4-10",">10 %")
 pal <- colorBin(
-  palette = "YlOrRd",
+  palette = purples,
   domain = donor$donor,
   na.color="#d0cccf",
   bins = palbins
@@ -104,12 +116,14 @@ pal <- colorBin(
 
 
 donor_map = ggplot(ug.f)+
-  geom_polygon( aes(x=long,y=lat,group=group,fill=donor,color="grey",size=0.02))+
+  geom_polygon( aes(x=long,y=lat,group=group,fill=donor,color="#eeeeee",size=0.21))+
   coord_fixed(1) +
   scale_fill_gradientn(
+    na.value="#d0cccf",
     guide="legend",
-    breaks=unname(palbins),
-    colors=pal(palbins)
+    breaks=palbins,
+    colors=pal(palbins),
+    values=rescale(palbins)
   ) +
   scale_color_identity()+
   scale_size_identity()+
@@ -121,7 +135,8 @@ donor_map = ggplot(ug.f)+
 ggsave(paste0("eps/donor.png"),donor_map,device="png",width=10,height=6)
 ggsave(paste0("eps/donor.eps"),donor_map,device="eps",width=10,height=6)
 
-palbins = quantile(dist.dat$fdi,seq(0,1,.2))
+palbins = c(5,20,36)
+names(palbins) = c("<5 %","5-20",">20 %")
 pal <- colorBin(
   palette = "YlOrRd",
   domain = dist.dat$fdi,
@@ -131,12 +146,14 @@ pal <- colorBin(
 
 
 fdi_map = ggplot(ug.f)+
-  geom_polygon( aes(x=long,y=lat,group=group,fill=fdi,color="grey",size=0.02))+
+  geom_polygon( aes(x=long,y=lat,group=group,fill=fdi,color="#eeeeee",size=0.21))+
   coord_fixed(1) +
   scale_fill_gradientn(
+    na.value="#d0cccf",
     guide="legend",
-    breaks=unname(palbins),
-    colors=pal(palbins)
+    breaks=palbins,
+    colors=pal(palbins),
+    values=rescale(palbins)
   ) +
   scale_color_identity()+
   scale_size_identity()+
